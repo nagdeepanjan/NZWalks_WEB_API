f125a1020dd1d5de2355222700d905eecab0d0c9
nagdeepanjan <mail@deepanjannag.com>
2026-01-31 19:26:00 -0500

    Add global exception handler middleware
    Introduced ExceptionHandlerMiddleware to catch and log unhandled exceptions during HTTP requests. Returns a standardized JSON error response with status code 500 and a generic error message to the client.

---
+++ NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace NZWalks.API.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+        private readonly RequestDelegate next;
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger, RequestDelegate next)
+        {
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                //Return a custom error response
+                logger.LogError(ex, ex.Message);
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+
+                var error = new { ErrorMessage= "Something went wrong. Check logs"};
+
+                await httpContext.Response.WriteAsJsonAsync(error);
+            }
+        }
+    }
+}
